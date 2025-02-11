ca26ed0d123797cb646219b6a1b526aeb820a97e
VIWARR <dm.pavlyuchenko@gmail.com>
2025-02-11 10:45:47 +0300

    total sales for the week

--- fruitreal-bi-analysis-sql/Scripts/sales-plan.sql
+++ fruitreal-bi-analysis-sql/Scripts/sales-plan.sql
@@ -1,6 +1,7 @@
 SELECT
-    year,
     product_name,
+    
+    -- Сумма продаж по дням
     COALESCE(SUM(CASE WHEN day_of_week = 'понедельник' THEN total_sales_kg END), 0) AS Monday,
     COALESCE(SUM(CASE WHEN day_of_week = 'вторник' THEN total_sales_kg END), 0) AS Tuesday,
     COALESCE(SUM(CASE WHEN day_of_week = 'среда' THEN total_sales_kg END), 0) AS Wednesday,
@@ -8,7 +9,14 @@ SELECT
     COALESCE(SUM(CASE WHEN day_of_week = 'пятница' THEN total_sales_kg END), 0) AS Friday,
     COALESCE(SUM(CASE WHEN day_of_week = 'суббота' THEN total_sales_kg END), 0) AS Saturday,
     COALESCE(SUM(CASE WHEN day_of_week = 'воскресенье' THEN total_sales_kg END), 0) AS Sunday,
+    
+    -- Итоговая сумма продаж за неделю
+    COALESCE(SUM(total_sales_kg), 0) AS Total_sales,
+    
+    -- План продаж за неделю
     COALESCE(MAX(sales_plan), 0) AS Sales_plan
+    
 FROM vp
+WHERE year = 2024
 GROUP BY year, product_name
 ORDER BY year, product_name;
